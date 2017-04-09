d8ee9e1f81ff83eb5cb604a7c97888b61b8a1789
Александр Витер <viter.alex@gmail.com>
2017-04-09 13:48:28 +0300

    Для темы http://www.cyberforum.ru/windows-forms/thread1543265.html

---
+++ Curlicue Fractal/MainForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace WindowsFormsApplication4 {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.stepNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stepNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(715, 401);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.rNumericUpDown);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.sNumericUpDown);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Controls.Add(this.tNumericUpDown);
+            this.flowLayoutPanel1.Controls.Add(this.label4);
+            this.flowLayoutPanel1.Controls.Add(this.stepNumericUpDown);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 369);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(709, 29);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "r:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // rNumericUpDown
+            // 
+            this.rNumericUpDown.DecimalPlaces = 1;
+            this.rNumericUpDown.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.rNumericUpDown.Location = new System.Drawing.Point(21, 3);
+            this.rNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.rNumericUpDown.Name = "rNumericUpDown";
+            this.rNumericUpDown.Size = new System.Drawing.Size(46, 20);
+            this.rNumericUpDown.TabIndex = 1;
+            this.rNumericUpDown.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.rNumericUpDown.ValueChanged += new System.EventHandler(this.rNumericUpDown_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(73, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "s:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // sNumericUpDown
+            // 
+            this.sNumericUpDown.DecimalPlaces = 1;
+            this.sNumericUpDown.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            65536});
+            this.sNumericUpDown.Location = new System.Drawing.Point(91, 3);
+            this.sNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            262144});
+            this.sNumericUpDown.Name = "sNumericUpDown";
+            this.sNumericUpDown.Size = new System.Drawing.Size(46, 20);
+            this.sNumericUpDown.TabIndex = 3;
+            this.sNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.sNumericUpDown.ValueChanged += new System.EventHandler(this.rNumericUpDown_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(143, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "t:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tNumericUpDown
+            // 
+            this.tNumericUpDown.DecimalPlaces = 1;
+            this.tNumericUpDown.Location = new System.Drawing.Point(161, 3);
+            this.tNumericUpDown.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.tNumericUpDown.Name = "tNumericUpDown";
+            this.tNumericUpDown.Size = new System.Drawing.Size(46, 20);
+            this.tNumericUpDown.TabIndex = 5;
+            this.tNumericUpDown.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.tNumericUpDown.ValueChanged += new System.EventHandler(this.rNumericUpDown_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(213, 0);
+            this.label4.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Шаг:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // stepNumericUpDown
+            // 
+            this.stepNumericUpDown.Location = new System.Drawing.Point(247, 3);
+            this.stepNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.stepNumericUpDown.Name = "stepNumericUpDown";
+            this.stepNumericUpDown.Size = new System.Drawing.Size(46, 20);
+            this.stepNumericUpDown.TabIndex = 7;
+            this.stepNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.stepNumericUpDown.ValueChanged += new System.EventHandler(this.rNumericUpDown_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(709, 360);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 401);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "Фрактал Курликю";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.rNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stepNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown rNumericUpDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown sNumericUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown tNumericUpDown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown stepNumericUpDown;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Curlicue Fractal/MainForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4 {
+    public partial class MainForm : Form {
+        Point origin;
+        public MainForm() {
+            InitializeComponent();
+            panel1.MouseClick += (s, e) => {
+                origin = e.Location;
+                panel1.Refresh();
+            };
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) {
+            DrawFractal(e.Graphics,
+                (float)rNumericUpDown.Value,
+                (float)sNumericUpDown.Value,
+                (float)tNumericUpDown.Value,
+                (int)stepNumericUpDown.Value,
+                origin);
+        }
+
+private void DrawFractal(Graphics g, float r, float s, float t, int step, Point origin) {
+    g.TranslateTransform(origin.X, origin.Y);
+    float angle;
+    int red, green, blue;
+    using (Pen pen = new Pen(Color.Black, 1.5f)) {
+        for (int v = -10000; v < 10000; v += step) {
+            angle = (float)(s * Math.Pow(v, t));
+            red = Math.Abs(v / 4 + 1024) % 255;
+            green = Math.Abs(255 - v / 3) % 255;
+            blue = Math.Abs(128 - v) % 255;
+            pen.Color = Color.FromArgb(red, green, blue);
+            g.DrawLine(pen, 0, 0, r, r);
+            g.TranslateTransform(r, r);
+            g.RotateTransform(angle);
+            Application.DoEvents();
+        }
+    }
+}
+
+        private void button1_Click(object sender, EventArgs e) {
+            panel1.Refresh();
+        }
+
+        private void rNumericUpDown_ValueChanged(object sender, EventArgs e) {
+            panel1.Refresh();
+        }
+    }
+}
