fd6cd225c3c30d11170582c26531be1c6f907844
Александр Витер <viter.alex@gmail.com>
2017-04-09 04:21:54 +0300

    Для темы http://www.cyberforum.ru/windows-forms/thread1894977.html

---
+++ WorkWithTextFile/MainForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace WorkWithTextFile
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.openToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.closeButton = new System.Windows.Forms.ToolStripButton();
+            this.saveToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.exportExcelButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.cutToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.copyToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.pasteToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.insertRowButton = new System.Windows.Forms.ToolStripButton();
+            this.deleteRowButton = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openTxtFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripButton,
+            this.closeButton,
+            this.saveToolStripButton,
+            this.exportExcelButton,
+            this.toolStripSeparator,
+            this.cutToolStripButton,
+            this.copyToolStripButton,
+            this.pasteToolStripButton,
+            this.toolStripSeparator1,
+            this.insertRowButton,
+            this.deleteRowButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(707, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // openToolStripButton
+            // 
+            this.openToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.openToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripButton.Image")));
+            this.openToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripButton.Name = "openToolStripButton";
+            this.openToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.openToolStripButton.Text = "&Открыть...";
+            this.openToolStripButton.Click += new System.EventHandler(this.openToolStripButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.closeButton.Image = global::WorkWithTextFile.Properties.Resources.Close;
+            this.closeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(23, 22);
+            this.closeButton.Text = "Закрыть";
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // saveToolStripButton
+            // 
+            this.saveToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.saveToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripButton.Image")));
+            this.saveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveToolStripButton.Name = "saveToolStripButton";
+            this.saveToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.saveToolStripButton.Text = "&Сохранить";
+            this.saveToolStripButton.Click += new System.EventHandler(this.saveToolStripButton_Click);
+            // 
+            // exportExcelButton
+            // 
+            this.exportExcelButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.exportExcelButton.Image = global::WorkWithTextFile.Properties.Resources.ExportToExcel;
+            this.exportExcelButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.exportExcelButton.Name = "exportExcelButton";
+            this.exportExcelButton.Size = new System.Drawing.Size(23, 22);
+            this.exportExcelButton.Text = "Экспорт в Excel";
+            this.exportExcelButton.Click += new System.EventHandler(this.exportExcelButton_Click);
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // cutToolStripButton
+            // 
+            this.cutToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cutToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("cutToolStripButton.Image")));
+            this.cutToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cutToolStripButton.Name = "cutToolStripButton";
+            this.cutToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.cutToolStripButton.Text = "Вырезать";
+            this.cutToolStripButton.Click += new System.EventHandler(this.cutToolStripButton_Click);
+            // 
+            // copyToolStripButton
+            // 
+            this.copyToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.copyToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("copyToolStripButton.Image")));
+            this.copyToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.copyToolStripButton.Name = "copyToolStripButton";
+            this.copyToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.copyToolStripButton.Text = "Копировать";
+            this.copyToolStripButton.Click += new System.EventHandler(this.copyToolStripButton_Click);
+            // 
+            // pasteToolStripButton
+            // 
+            this.pasteToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.pasteToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("pasteToolStripButton.Image")));
+            this.pasteToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.pasteToolStripButton.Name = "pasteToolStripButton";
+            this.pasteToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.pasteToolStripButton.Text = "Вставить";
+            this.pasteToolStripButton.Click += new System.EventHandler(this.pasteToolStripButton_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // insertRowButton
+            // 
+            this.insertRowButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.insertRowButton.Image = global::WorkWithTextFile.Properties.Resources.InsertRow;
+            this.insertRowButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.insertRowButton.Name = "insertRowButton";
+            this.insertRowButton.Size = new System.Drawing.Size(23, 22);
+            this.insertRowButton.Text = "Вставить строку";
+            this.insertRowButton.Click += new System.EventHandler(this.insertRowButton_Click);
+            // 
+            // deleteRowButton
+            // 
+            this.deleteRowButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.deleteRowButton.Image = global::WorkWithTextFile.Properties.Resources.DeleteRow;
+            this.deleteRowButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.deleteRowButton.Name = "deleteRowButton";
+            this.deleteRowButton.Size = new System.Drawing.Size(23, 22);
+            this.deleteRowButton.Text = "Удалить строку";
+            this.deleteRowButton.Click += new System.EventHandler(this.deleteRowButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(707, 521);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.ColumnAdded += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dataGridView1_ColumnAdded);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // openTxtFileDialog
+            // 
+            this.openTxtFileDialog.Filter = "Текстовые файлы|*.txt";
+            this.openTxtFileDialog.Title = "Выберите файл";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 546);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "MainForm";
+            this.Text = "Редактирование файла";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton openToolStripButton;
+        private System.Windows.Forms.ToolStripButton saveToolStripButton;
+        private System.Windows.Forms.ToolStripButton exportExcelButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripButton cutToolStripButton;
+        private System.Windows.Forms.ToolStripButton copyToolStripButton;
+        private System.Windows.Forms.ToolStripButton pasteToolStripButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openTxtFileDialog;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton insertRowButton;
+        private System.Windows.Forms.ToolStripButton deleteRowButton;
+        private System.Windows.Forms.ToolStripButton closeButton;
+    }
+}
+
---
+++ WorkWithTextFile/MainForm.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WorkWithTextFile
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            dataGridView1.AutoGenerateColumns = true;
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            exportExcelButton.Enabled = dataGridView1.RowCount > 0;
+            cutToolStripButton.Enabled = dataGridView1.CurrentCell != null;
+            copyToolStripButton.Enabled = dataGridView1.CurrentCell != null;
+            pasteToolStripButton.Enabled = dataGridView1.CurrentCell != null;
+            deleteRowButton.Enabled = dataGridView1.CurrentRow != null;
+            insertRowButton.Enabled = dataGridView1.CurrentRow != null;
+            saveToolStripButton.Enabled = !string.IsNullOrEmpty(_filename) && dataGridView1.RowCount > 0;
+
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            openTxtFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            if (openTxtFileDialog.ShowDialog(this) != DialogResult.OK) return;
+            _filename = openTxtFileDialog.FileName;
+            ReadFile(openTxtFileDialog.FileName);
+        }
+
+        private List<List<string>> _dataList;
+        private string _filename;
+
+        private void ReadFile(string fileName)
+        {
+            _dataList = new List<List<string>>();
+            using (var reader = new StreamReader(fileName, Encoding.Default))
+            {
+                while (reader.Peek() != -1)
+                {
+                    _dataList.Add(new List<string>(reader.ReadLine().Split(' ')));
+                }
+            }
+            UpdateDgv();
+        }
+
+        private void UpdateDgv()
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.ColumnCount = _dataList[0].Count;
+            foreach (List<string> list in _dataList)
+            {
+                dataGridView1.Rows.Add(list.ToArray());
+            }
+            UpdateView();
+        }
+
+        private void cutToolStripButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell == null)
+            {
+                return;
+            }
+            if (dataGridView1.CurrentCell.FormattedValue != null)
+                Clipboard.SetText(dataGridView1.CurrentCell.FormattedValue.ToString());
+            dataGridView1.CurrentCell.Value = null;
+        }
+
+        private void copyToolStripButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell == null)
+            {
+                return;
+            }
+            if (dataGridView1.CurrentCell.FormattedValue != null)
+                Clipboard.SetText(dataGridView1.CurrentCell.FormattedValue.ToString());
+        }
+
+        private void pasteToolStripButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell == null)
+            {
+                return;
+            }
+            dataGridView1.CurrentCell.Value = Clipboard.GetText();
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            using (var stream = new StreamWriter(_filename, false, Encoding.Default))
+            {
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    var list = new List<string>();
+                    for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                    {
+                        list.Add(dataGridView1[i, j].Value.ToString());
+                    }
+                    stream.WriteLine(string.Join(" ", list));
+                }
+            }
+        }
+
+        private void exportExcelButton_Click(object sender, EventArgs e)
+        {
+            dynamic xlApp = Activator.CreateInstance(Type.GetTypeFromProgID("Excel.Application"));
+            xlApp.Visible = true;
+            xlApp.Workbooks.Add();
+            var cell = dataGridView1.CurrentCell;
+            dataGridView1.SelectAll();
+            Clipboard.SetDataObject(dataGridView1.GetClipboardContent());
+            xlApp.ActiveSheet.Paste();
+            if (cell != null)
+            {
+                dataGridView1.CurrentCell = cell;
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            UpdateView();
+        }
+
+        private void insertRowButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+            var col = dataGridView1.CurrentCell.ColumnIndex;
+            var index = dataGridView1.CurrentRow.Index;
+            dataGridView1.Rows.Insert(index, 1);
+            dataGridView1.CurrentCell = dataGridView1[col, index];
+        }
+
+        private void deleteRowButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+            dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+        }
+
+        private void dataGridView1_ColumnAdded(object sender, DataGridViewColumnEventArgs e)
+        {
+            e.Column.HeaderText = String.Format("Столбец {0}", e.Column.Index + 1);
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            _filename = string.Empty;
+            if (_dataList != null) _dataList.Clear();
+            dataGridView1.Columns.Clear();
+            UpdateView();
+        }
+    }
+}
