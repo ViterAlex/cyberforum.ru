2a47c4489b9cd74c4d3d56569f65e8b4d8f9b0da
Александр Витер <viter.alex@gmail.com>
2017-04-09 04:05:15 +0300

    Для темы http://www.cyberforum.ru/vb-net/thread1936479.html

---
+++ RasteriziAlgorithmes/MainForm.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.FlowLayoutPanel1, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(800, 548)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(148, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(649, 542)
+        Me.Panel1.TabIndex = 0
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoSize = True
+        Me.FlowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.FlowLayoutPanel1.Controls.Add(Me.TableLayoutPanel2)
+        Me.FlowLayoutPanel1.Controls.Add(Me.ComboBox1)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(3, 3)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(139, 542)
+        Me.FlowLayoutPanel1.TabIndex = 1
+        Me.FlowLayoutPanel1.WrapContents = False
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.AutoSize = True
+        Me.TableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.TableLayoutPanel2.ColumnCount = 2
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.NumericUpDown1, 1, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label1, 0, 0)
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 3)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(133, 26)
+        Me.TableLayoutPanel2.TabIndex = 3
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(74, 3)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(56, 20)
+        Me.NumericUpDown1.TabIndex = 1
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {15, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 26)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Размер клетки"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Безенхайм", "ЦДА"})
+        Me.ComboBox1.Location = New System.Drawing.Point(3, 35)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 548)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "MainForm"
+        Me.Text = "Алгоритмы растеризации"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents Label1 As Label
+End Class
---
+++ RasteriziAlgorithmes/MainForm.vb
@@ -0,0 +1,180 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Class MainForm
+#Region " Алгоритм "
+    'Реализация алгоритма Брезенхайма
+    'https://habrahabr.ru/post/185086/
+    Private Shared Iterator Function BresenhamPoints(pt1 As Point, pt2 As Point, w As Integer) As IEnumerable(Of Point)
+        Dim x0 = pt1.X, y0 = pt1.Y, x1 = pt2.X, y1 = pt2.Y
+        Dim dir = Math.Abs(y1 - y0) > Math.Abs(x1 - x0)
+        If dir Then
+            Swap(x0, y0)
+            Swap(x1, y1)
+        End If
+        If x0 > x1 Then
+            Swap(x0, x1)
+            Swap(y0, y1)
+        End If
+        Dim dx = x1 - x0, dy = Math.Abs(y1 - y0)
+        Dim signY = Math.Sign(y1 - y0)
+        Dim y = y0
+        Dim err = dx >> 1
+        For x = x0 To x1 Step w
+            Yield New Point(IIf(dir, y, x), IIf(dir, x, y))
+            err -= dy
+            If err < 0 Then
+                y += signY * w
+                err += dx
+            End If
+        Next
+    End Function
+
+    'Обмен значений между переменными
+    Private Shared Sub Swap(ByRef x0 As Integer, ByRef y0 As Integer)
+        Dim temp = x0
+        x0 = y0
+        y0 = temp
+    End Sub
+
+    'Реализация алгоритма ЦДА
+    Private Shared Iterator Function DdaPoints(pt1 As Point, pt2 As Point, w As Integer) As IEnumerable(Of Point)
+        Dim x0 = pt1.X, y0 = pt1.Y, x1 = pt2.X, y1 = pt2.Y
+        Dim dir = Math.Abs(y1 - y0) > Math.Abs(x1 - x0)
+        If dir Then
+            Swap(x0, y0)
+            Swap(x1, y1)
+        End If
+        If x0 > x1 Then
+            Swap(x0, x1)
+            Swap(y0, y1)
+        End If
+        Dim l = Math.Max(x1 - x0, y1 - y0) + 1.0F
+        Dim x As Single = x0, y As Single = y0
+        Dim dx = Math.Abs(x1 - x0) * w / l, dy = Math.Abs(y1 - y0) * w / l
+        Dim signX = Math.Sign(x1 - x0)
+        Dim signY = Math.Sign(y1 - y0)
+        For i = 0 To l Step w
+            Yield New Point(IIf(dir, y, x), IIf(dir, x, y))
+            x += signX * dx : y += signY * dy
+        Next
+    End Function
+#End Region
+
+#Region " Поля "
+    Private _width As Integer
+    'Переменная со ссылкой на алгоритм
+    Private _alg As Func(Of Point, Point, Integer, IEnumerable(Of Point))
+    Private _grid As GraphicsPath
+    'Первая точка прямой
+    Private _firstPoint As Point
+    'Вторая точка прямой
+    Private _secondPoint As Point
+    'Режим указания точек прямой
+    Private _pointMode As Boolean
+    Private ReadOnly _gridPen = New Pen(Color.Black, 1) With {.DashStyle = DashStyle.Dash}
+    Private ReadOnly _algs = New List(Of Func(Of Point, Point, Integer, IEnumerable(Of Point))) _
+        (New Func(Of Point, Point, Integer, IEnumerable(Of Point))() {
+                                                                         AddressOf BresenhamPoints,
+                                                                         AddressOf DdaPoints
+                                                                     }
+         )
+
+#End Region
+
+#Region " События формы "
+    'Рисование с алгоритмом
+    Private Sub Bresenham_Paint(sender As Object, e As PaintEventArgs)
+        'Если точек нет — выходим
+        If _firstPoint.IsEmpty OrElse _secondPoint.IsEmpty Then Return
+        'Если функция алгоритма не задана — выходим
+        If _alg Is Nothing Then Return
+        'Получаем точки растра по выбранному алгоритму
+        Dim list = _alg(_firstPoint, _secondPoint, _width).ToList()
+        Using fillBrush As New SolidBrush(Color.FromArgb(80, 255, 255, 0))
+            'Для каждой точки растра делаем заливку соответствующего квадрата
+            list.ForEach(Sub(point)
+                             Dim x = point.X \ _width
+                             Dim y = point.Y \ _width
+                             x *= _width
+                             y *= _width
+                             e.Graphics.FillRectangle(fillBrush, x, y, _width, _width)
+                             e.Graphics.DrawRectangle(Pens.Red, x, y, _width, _width)
+                         End Sub)
+        End Using
+        'Рисуем саму линию
+        e.Graphics.DrawLine(Pens.Blue, _firstPoint, _secondPoint)
+    End Sub
+
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        _firstPoint = e.Location
+        _pointMode = True
+        RemoveHandler Panel1.Paint, AddressOf Bresenham_Paint
+        AddHandler Panel1.Paint, AddressOf MouseMovePaint
+    End Sub
+
+    'Рисование линии при указании точек прямой
+    Private Sub MouseMovePaint(sender As Object, e As PaintEventArgs)
+        e.Graphics.DrawLine(Pens.Green, _firstPoint, _secondPoint)
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        RemoveHandler Panel1.Paint, AddressOf MouseMovePaint
+        AddHandler Panel1.Paint, AddressOf Bresenham_Paint
+        _pointMode = False
+        _width = NumericUpDown1.Value
+        Panel1.Invalidate()
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If Not _pointMode Then
+            Return
+        End If
+        _secondPoint = e.Location
+        Panel1.Invalidate()
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        _width = NumericUpDown1.Value
+        CreateGrid()
+        Panel1.Invalidate()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        _alg = _algs(ComboBox1.SelectedIndex)
+        Panel1.Invalidate()
+    End Sub
+
+    Protected Overrides Sub OnLoad(e As EventArgs)
+        MyBase.OnLoad(e)
+        ComboBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub Panel1_SizeChanged(sender As Object, e As EventArgs) Handles Panel1.SizeChanged
+        If (_width = 0) Then Return
+        CreateGrid()
+        Panel1.Invalidate()
+    End Sub
+
+    'Основной метод прорисовки панели. В нём просто рисуем сетку
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+        'Рисуем сетку
+        e.Graphics.DrawPath(_gridPen, _grid)
+    End Sub
+#End Region
+
+    'создание сетки
+    Private Sub CreateGrid()
+        _grid = New GraphicsPath()
+        For i = _width To Panel1.ClientSize.Width Step _width
+            _grid.StartFigure()
+            _grid.AddLine(i, 0, i, Panel1.ClientSize.Height)
+            _grid.CloseFigure()
+        Next
+        For i = _width To Panel1.ClientSize.Height Step _width
+            _grid.StartFigure()
+            _grid.AddLine(0, i, Panel1.ClientSize.Width, i)
+            _grid.CloseFigure()
+        Next
+    End Sub
+End Class
